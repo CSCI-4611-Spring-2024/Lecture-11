fd788fd094a9adfb2367b5e1345668ae7ec18416
Evan Suma Rosenberg <suma@umn.edu>
2024-02-20 15:22:16 -0600

    class update

--- src/App.ts
+++ src/App.ts
@@ -8,24 +8,34 @@ import * as gfx from 'gophergfx'
 
 export class App extends gfx.GfxApp
 {
+    private cameraControls: gfx.OrbitControls;
+
     // --- Create the App class ---
     constructor()
     {
         // initialize the base class gfx.GfxApp
         super();
+
+        this.cameraControls = new gfx.OrbitControls(this.camera);
     }
 
 
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        // Setup camera
+        this.camera.setPerspectiveCamera(60, 1920/1080, 0.1, 10);
 
+        this.cameraControls.setDistance(3);
+        this.cameraControls.setOrbit(gfx.MathUtils.degreesToRadians(-22.5), 0);
+        const axes = gfx.Geometry3Factory.createAxes();
+        this.scene.add(axes);
     }
 
     
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-
+        this.cameraControls.update(deltaTime);
     }
 }
